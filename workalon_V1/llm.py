aba8859033d122b05d8d9fba57cb476476f5ef64
quinoha <noah.kwon@stonybrook.edu>
2026-05-14 18:34:07 +0900

    gemini api

--- workalon_V1/llm.py
+++ workalon_V1/llm.py
@@ -1,20 +1,60 @@
-import google.generativeai as genai
-from config import * 
 import os
+import json
+import argparse
+from dotenv import load_dotenv
+from google import genai
+
 
 """
 Use Gemini API for workout routine recommendation
-prompt is as follows:
+
     
 """
 
-genai.configure(api_key=GEMINI_API_KEY)
+load_dotenv()
+your_api_key = os.getenv("GEMINI_API_KEY")
 
-# 2. 모델 설정
-model = genai.GenerativeModel('gemini-2.5-flash') # 혹은 최신 버전 사용
 
-# 3. 콘텐츠 생성 (오타 주의: generate_content)
-response = model.generate_content("Recommend a workout routine for a 24 year-old male, 185cm, 80kg.")
+class WorkoutPlanner:
+    def __init__(self):
+        self.client = genai.Client(api_key=your_api_key)
 
-# 4. 결과 출력
-print(response.text)
+    def get_recommendation(self, user_specs, available_exercises):
+        """
+        Function for getting recommendation from Gemini
+        """
+
+        # read user profile .json file
+        with open('user_profile.json', 'r', encoding='utf-8') as file:
+            data = json.load(file)
+        print(data)
+
+
+        prompt = f"""
+        
+        [user specs]
+        gender: {user_specs['gender']}
+        height: {user_specs['height']}cm
+        weight: {user_specs['weight']}kg
+
+        [available exercise list]
+        {', '.join(available_exercises)}
+
+        [Output rules - Strict]
+        ONLY use words in
+        No need for any other explanation, ONLY lower-case english strings should be output.
+        ex: curl, squat, pushup
+
+        """
+
+        response = self.client.models.generate_content(
+            model='gemini-2.5-flash',
+            contents=prompt,
+        )
+
+        return response.text.strip().lower()
+
+
+# test code
+planner = WorkoutPlanner()
+result = planner.get_recommendation()
--- workalon_V1/llm_test.py
+++ workalon_V1/llm_test.py
@@ -1,6 +1,6 @@
-import google.generativeai as genai
-from config import * 
 import os
+from dotenv import load_dotenv
+from google import genai
 
 """
 Use Gemini API for workout routine recommendation
@@ -8,11 +8,23 @@ prompt is as follows:
 
 """
 
-genai.configure(api_key=GEMINI_API_KEY)
+# Load api key
+load_dotenv()
+gemini_api_key = os.getenv("GEMINI_API_KEY")
 
 # 1. Choose model
-model = genai.GenerativeModel('gemini-2.5-flash') # 혹은 최신 버전 사용
+model = genai.Client(api_key=gemini_api_key)
+
+
 # 2. Prompt and response
-response = model.generate_content("Recommend a workout routine for a 24 year-old male, 185cm, 80kg.")
+prompt = '''[Output rules - Strict]
+
+            ONLY use words in No need for any other explanation,
+            ONLY lower-case english strings should be output. ex: curl, squat, pushup
+            recommend a workout routine for male, 185cm, 80kg             
+            '''
+
+r = model.models.generate_content(model='gemini-2.5-flash', contents=prompt)
+
 # 3. Print response
-print(response.text)
+print(r)
